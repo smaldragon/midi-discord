24334c6a382f603be3c30fdcc9befcd498d8ea3f
smal <6433336+smaldragon@users.noreply.github.com>
2021-12-26 17:06:12 +0000

    Various Improvements * `config.py` device_name can now be a substring of the full name * added more comments and moved variables around

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 from pynput.keyboard import Key
 # Uncomment these lines with your own values
+# device_name can be a sub-string of the full device name
 # KeyCodes can be found at https://pynput.readthedocs.io/en/latest/keyboard.html#pynput.keyboard.Key
 
-#device_name = "i2 mini 32 MIDI 1"
+#device_name = "i2 mini 32"
 #push_key = Key.scroll_lock
 #inverted = True
--- main.py
+++ main.py
@@ -2,23 +2,41 @@
 import mido
 from pynput import keyboard
 
+# == midi-discord variables == #
 device_name = ""
 push_key = None
 inverted = False
+input_names = mido.get_input_names()
 
+# ============================
+# Setup Variables
+# ============================
+# from config.py
 try:
   from config import device_name,push_key,inverted
   print("Loading from config.py")
+  if device_name not in input_names:
+    found = False
+    for device in input_names:
+      if device_name in device:
+        print("Successfully connected via config.py")
+        device_name = device
+        found = True
+        break
+    if not found:
+      print("ERROR: Failed to connect via config.py")
+      raise Exception
+# from user input
 except:
   print("Select your Configuration")
-  input_names = mido.get_input_names()
   input_devices = []
-  # Looping logic as get_input_names returns duplicated entries
+  # Looping logic as get_input_names can return duplicated entries
   for name in input_names:
     if name not in input_devices:
       input_devices.append(name)
       print(f'{len(input_devices)}. {name}')
   selected=0
+  # Select the device
   while True:  
     try:
       selected = int(input("Choose pedal device (negative number for reversed polarity): "))
@@ -50,7 +68,10 @@ except:
     pass
   listener.join(0.1)
 
-# Detect if pedal (cc64) is pressed or lifted and act accordingly
+# ============================
+# Main Loop, detect if pedal (cc64) is pressed or lifted and act accordingly
+# ============================
+print(f"Connectig to {device_name}")
 with mido.open_input(device_name) as inport:
   spoof_keyboard = keyboard.Controller()
   for msg in inport:
